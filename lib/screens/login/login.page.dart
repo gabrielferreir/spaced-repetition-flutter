423bb6a3ff939c14d25eea54152f3b46b27c8af8
Gabriel Ferreira <gabrielferrer@outlook.com.br>
2018-12-21 10:01:38 -0200

    Dividindo tela de scroll

--- lib/screens/login/login.page.dart
+++ lib/screens/login/login.page.dart
@@ -7,77 +7,181 @@ class LoginPage extends StatefulWidget {
   _LoginPageState createState() => _LoginPageState();
 }
 
-class _LoginPageState extends State<LoginPage> {
-  TextEditingController emailController = TextEditingController();
+class _LoginPageState extends State<LoginPage> with SingleTickerProviderStateMixin {
+  final controller = new PageController();
+  final emailController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisSize: MainAxisSize.max,
-        children: <Widget>[
-          Flexible(
-            child: Container(
-              color: Theme.of(context).primaryColor,
-              child: Center(
-                child: Text('Logo'),
-              ),
-              height: 240.0,
+        body: Column(
+      mainAxisSize: MainAxisSize.max,
+      children: <Widget>[
+        Flexible(
+          child: Container(
+            color: Theme.of(context).primaryColor,
+            child: Center(
+              child: Text('Logo'),
             ),
+            height: 240.0,
           ),
-          Row(
+        ),
+        Expanded(
+          child: PageView(
+            controller: controller,
             children: <Widget>[
-              Flexible(
-                child: Padding(
-                  padding: EdgeInsets.all(16.0),
-                  child: TextFormField(
-                    autofocus: true,
-                    controller: emailController,
-                    decoration: InputDecoration(labelText: 'Email'),
-                  ),
+              _email(emailController, _nextPage),
+              _pass(emailController, _prevPage),
+            ],
+          ),
+        )
+      ],
+    ));
+  }
+
+  _nextPage() {
+    controller.nextPage(duration: Duration(milliseconds: 280), curve: Curves.ease);
+  }
+
+  _prevPage() {
+    controller.previousPage(duration: Duration(milliseconds: 280), curve: Curves.ease);
+  }
+
+}
+
+class _email extends StatefulWidget {
+  TextEditingController emailController;
+  Function callback;
+
+  @override
+  __emailState createState() => __emailState();
+
+  _email(TextEditingController this.emailController, Function this.callback);
+}
+
+class __emailState extends State<_email> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Row(
+          children: <Widget>[
+            Flexible(
+              child: Padding(
+                padding: EdgeInsets.all(16.0),
+                child: TextFormField(
+                  autofocus: true,
+                  controller: this.widget.emailController,
+                  decoration: InputDecoration(labelText: 'Email'),
                 ),
               ),
-            ],
-          ),
-          Row(
-            mainAxisSize: MainAxisSize.max,
-            children: <Widget>[
-              Flexible(
-                  child: Padding(
-                      padding: EdgeInsets.all(16.0),
-                      child: ButtonTheme(
-                        minWidth: double.infinity,
-                        child: FlatButton(
-                          onPressed: () {
-                            Navigator.push(context, SlideRouterRight(widget: HomePage()));
-                          },
-                          color: Theme.of(context).primaryColor,
-                          child: Text(
-                            'ENTRAR',
-                            style: TextStyle(color: Colors.white),
-                          ),
+            ),
+          ],
+        ),
+        Row(
+          mainAxisSize: MainAxisSize.max,
+          children: <Widget>[
+            Flexible(
+                child: Padding(
+                    padding: EdgeInsets.all(16.0),
+                    child: ButtonTheme(
+                      minWidth: double.infinity,
+                      height: 48.0,
+                      child: FlatButton(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(48.0)),
+                        onPressed: () {
+                          widget.callback();
+                        },
+                        color: Theme.of(context).primaryColor,
+                        child: Text(
+                          'ENTRAR',
+                          style: TextStyle(color: Colors.white),
                         ),
-                      ))),
-            ],
-          ),
-        ],
-      ),
+                      ),
+                    ))),
+          ],
+        ),
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text('Desejo me cadastrar'),
+            )
+          ],
+        )
+      ],
     );
   }
 }
 
-//Positioned(
-//bottom: 0.0,
-//left: 0.0,
-//child: SizedBox.expand(
-//child: Padding(
-//padding: EdgeInsets.all(0.0),
-//child: FlatButton(
-//onPressed: () {},
-//color: Theme.of(context).primaryColor,
-//child: Text(
-//'ENTRAR',
-//style: TextStyle(color: Colors.white),
-//))),
-//),
-//)
+class _pass extends StatefulWidget {
+  TextEditingController emailController;
+  Function callback;
+
+  @override
+  __passState createState() => __passState();
+
+  _pass(TextEditingController this.emailController, Function this.callback);
+}
+
+class __passState extends State<_pass> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Row(
+          children: <Widget>[
+            Flexible(
+              child: Padding(
+                padding: EdgeInsets.all(16.0),
+                child: TextFormField(
+                  autofocus: true,
+                  controller: this.widget.emailController,
+                  decoration: InputDecoration(labelText: 'Senha'),
+                ),
+              ),
+            ),
+          ],
+        ),
+        Row(
+          mainAxisSize: MainAxisSize.max,
+          children: <Widget>[
+            Flexible(
+                child: Padding(
+                    padding: EdgeInsets.all(16.0),
+                    child: ButtonTheme(
+                      minWidth: double.infinity,
+                      height: 48.0,
+                      child: FlatButton(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(48.0)),
+                        onPressed: () {
+                          widget.callback();
+                        },
+                        color: Theme.of(context).primaryColor,
+                        child: Text(
+                          'ENTRAR',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    ))),
+          ],
+        ),
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text('Desejo me cadastrar'),
+            )
+          ],
+        )
+      ],
+    );
+  }
+}
+
