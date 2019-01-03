455a044f85288e45d74b4893d3eecfa63dd316f4
Gabriel Ferreira <gabrielferrer@outlook.com.br>
2019-01-03 08:34:16 -0200

    Dividindo login

---
+++ lib/screens/login/email.widget.dart
@@ -0,0 +1,127 @@
+import 'package:flutter/material.dart';
+import 'package:tg/screens/register/register.page.dart';
+import 'package:tg/router/slide_router_right.dart';
+
+class Email extends StatefulWidget {
+  TextEditingController emailController;
+  Function callback;
+  FocusNode focusNode;
+
+  @override
+  _EmailState createState() => _EmailState();
+
+  Email(TextEditingController this.emailController, FocusNode this.focusNode,
+      Function this.callback);
+}
+
+class _EmailState extends State<Email> {
+  bool validateInClient = true;
+  bool dirty = false;
+  String lastValue = '';
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  void initState() {
+    this.widget.emailController.addListener(() {
+      setState(() {
+        if (lastValue != widget.emailController.text) {
+          dirty = true;
+        }
+        validateInClient = true;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          Row(
+            children: <Widget>[
+              Flexible(
+                child: Padding(
+                    padding: EdgeInsets.all(16.0),
+                    child: Form(
+                      key: _formKey,
+                      child: TextFormField(
+                          autofocus: true,
+                          focusNode: this.widget.focusNode,
+                          controller: this.widget.emailController,
+                          decoration: InputDecoration(labelText: 'Email'),
+                          autovalidate: validateInClient,
+                          validator: (value) {
+                            return validateInClient
+                                ? validatorClient(value)
+                                : validatorServer(value);
+                          }),
+                    )),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisSize: MainAxisSize.max,
+            children: <Widget>[
+              Flexible(
+                  child: Padding(
+                      padding: EdgeInsets.all(16.0),
+                      child: ButtonTheme(
+                        minWidth: double.infinity,
+                        height: 48.0,
+                        child: FlatButton(
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(48.0)),
+                          onPressed: () {
+                            dirty = true;
+                            if (_formKey.currentState.validate()) {
+                              validateInClient = !validateInClient;
+                              if (_formKey.currentState.validate()) {
+                                widget.callback();
+                              }
+                            }
+                          },
+                          color: Theme.of(context).primaryColor,
+                          child: Text(
+                            'ENTRAR',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      ))),
+            ],
+          ),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: GestureDetector(
+                  child: Text('Desejo me cadastrar'),
+                  onTap: () {
+                    Navigator.push(context,
+                        SlideRouterRight(widget: RegisterPage()));
+                  },
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  validatorServer(String value) {
+    if (value != 'pazuzu@gmail.com') {
+      return 'Digite um email existente';
+    }
+  }
+
+  validatorClient(String value) {
+    if (value.isEmpty && dirty) {
+      return 'Email é obrigatorio';
+    } else if (dirty &&
+        !RegExp(r"^[a-zA-Z0-9.]+@[a-zA-Z0-9]+\.[a-zA-Z]+").hasMatch(value)) {
+      return 'Digite um email válido';
+    }
+  }
+}
---
+++ lib/screens/login/pass.widget.dart
@@ -0,0 +1,159 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:tg/router/slide_router_right.dart';
+import 'package:tg/screens/home/home.page.dart';
+
+class Pass extends StatefulWidget {
+  TextEditingController passController;
+  Function callback;
+  FocusNode focusNode;
+
+  @override
+  _PassState createState() => _PassState();
+
+  Pass(TextEditingController this.passController, FocusNode this.focusNode,
+      Function this.callback);
+}
+
+class _PassState extends State<Pass> {
+  bool validateInClient = true;
+  bool dirty = false;
+  String lastValue = '';
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  void initState() {
+    this.widget.passController.addListener(() {
+      setState(() {
+        if (lastValue != widget.passController.text) {
+          dirty = true;
+        }
+        validateInClient = true;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Row(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    IconButton(
+                        icon: Icon(Icons.arrow_back),
+                        onPressed: () {
+                          this.widget.callback();
+                        }),
+                    CircleAvatar(
+                      child: Text('G'),
+                      backgroundColor: Theme.of(context).primaryColor,
+                    ),
+                    Padding(
+                        padding: EdgeInsets.only(left: 16.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              'Gabriel Ferreira',
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                            Text(
+                              'gabriel.ferreira@outlook.com.br',
+                              style: TextStyle(),
+                            ),
+                          ],
+                        ))
+                  ],
+                ),
+              )
+            ],
+          ),
+          Row(
+            children: <Widget>[
+              Flexible(
+                child: Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: Form(
+                    key: _formKey,
+                    child: TextFormField(
+                        focusNode: this.widget.focusNode,
+                        controller: this.widget.passController,
+                        decoration: InputDecoration(labelText: 'Senha'),
+                        autovalidate: validateInClient,
+                        validator: (value) {
+                          return validateInClient
+                              ? validatorClient(value)
+                              : validatorServer(value);
+                        }),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisSize: MainAxisSize.max,
+            children: <Widget>[
+              Flexible(
+                  child: Padding(
+                      padding: EdgeInsets.all(16.0),
+                      child: ButtonTheme(
+                        minWidth: double.infinity,
+                        height: 48.0,
+                        child: FlatButton(
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(48.0)),
+                          onPressed: () {
+                            setState(() {
+                              dirty = true;
+                            });
+                            if (_formKey.currentState.validate()) {
+                              validateInClient = !validateInClient;
+                              if (_formKey.currentState.validate()) {
+                                saveInStorage(widget.passController.text);
+                                Navigator.pushReplacement(context,
+                                    SlideRouterRight(widget: HomePage()));
+                              }
+                            }
+                          },
+                          color: Theme.of(context).primaryColor,
+                          child: Text(
+                            'ENTRAR',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      ))),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  validatorServer(String value) {
+    if (value != 'teste123') {
+      return 'Digite uma senha válida';
+    }
+  }
+
+  validatorClient(String value) {
+    if (value.isEmpty && dirty) {
+      return 'Senha é obrigatoria';
+    } else if (dirty && value.length < 8) {
+      return 'A senha deve conter no minimo 8 caracteres';
+    }
+  }
+
+  saveInStorage(String pass) {
+    final _storage = new FlutterSecureStorage();
+    _storage.write(key: 'token', value: pass);
+  }
+}
